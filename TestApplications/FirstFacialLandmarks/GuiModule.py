7326faf998281bdc8fe1be0f839c2b6d92e0be31
AlexanderP <Alexander.Preuss@uni-oldenburg.de>
2017-12-23 15:43:43 +0100

    Deleted useless files Gui is ready for improvement TODOS in GuiModule.py as comments

--- TestApplications/FirstFacialLandmarks/GuiModule.py
+++ TestApplications/FirstFacialLandmarks/GuiModule.py
@@ -2,10 +2,16 @@ import cv2
 from Tkinter import *
 from tkFileDialog import *
 from tkMessageBox import *
-from forPresentationOnly import *
+from PhotoLogic import *
 from PIL import ImageTk, Image
+import tkFont
 
-
+#TODOS UNTIL 01.01.18:
+    # 1. RESIZE THE IMAGE OF THE PREVIEW PHOTOS !DONE!
+    # 2. REWRITE METHOD FOR SQARE-FACE-DETECTION/PASTING
+    # 3. USE CANVAS OBJECTS FOR DRAG AND DROP OF THE MASKS/FACES, EASIER THAN USING A BAR
+    # 4. SAVE THE IMAGE IN AN OWN FOLDER
+    # 5. BE HAPPY :)
 def generateWindow():
     root = Tk()
     root.title("Group Photo Editor 1.0")
@@ -40,7 +46,7 @@ class GroupPhotoEditor:
         button2 = Button(bottomFrame,textvariable = secondImg, command = lambda: self.onButton2(master,secondImg)).pack(fill=BOTH)
         button3 = Button(bottomFrame,text = "Generate Image (masks)", command = self.onButton3).pack(fill=BOTH)
         button4 = Button(bottomFrame,text = "Generate Image (faces)", command = self.onButton4).pack(fill=BOTH)
-        button5 = Button(bottomFrame,text = "Generate Example", command = self.onButton5).pack(fill=BOTH)
+        button5 = Button(bottomFrame,text = "Generate Example", command = lambda: self.onButton5(master,firstImg,secondImg)).pack(fill=BOTH)
         button6 = Button(bottomFrame,text = "Quit", fg="red", command = master.destroy).pack(fill=BOTH)
         
       
@@ -67,15 +73,14 @@ class GroupPhotoEditor:
             self.firstPreviewLabel.destroy()
         # create an image by using the filename-URL
         tmp = Image.open(filename)
+        tmp = tmp.resize((450, 300), Image.ANTIALIAS)
         theImage = ImageTk.PhotoImage(tmp)
-        print("test") # DEBUGGING: Delete this when no longer needed
         # destroy the old label: delete all references to the old image
         self.firstPreviewLabel = Label(self.firstPreviewFrame, image = theImage,text ="Image 1", compound=CENTER,font=("Helvetica", 30))
         # make sure the references are correct:
         self.firstPreviewLabel.image = theImage
         # pack the label with out image in the frame for preview:
         self.firstPreviewLabel.pack(side = LEFT, fill = BOTH, expand = YES)
-
 
 
     def onButton2(self,master,secondImg):
@@ -98,8 +103,8 @@ class GroupPhotoEditor:
             self.secondPreviewLabel.destroy()
         # create an image by using the filename-URL
         tmp = Image.open(filename)
+        tmp = tmp.resize((450, 300), Image.ANTIALIAS)
         theImage = ImageTk.PhotoImage(tmp)
-        print("test") # DEBUGGING: Delete this when no longer needed
         # destroy the old label: delete all references to the old image
         self.secondPreviewLabel = Label(self.secondPreviewFrame, image = theImage,text ="Image 2", compound=CENTER,font=("Helvetica", 30))
         # make sure the references are correct:
@@ -109,28 +114,76 @@ class GroupPhotoEditor:
     def onButton3(self):
         imageObj = Image.open(self.initialImg)
         image = cv2.imread(self.targetImg)
-        cv2.imshow('AUSGANGSBILD 1', image)
-        cv2.imshow('AUSGANGSBILD 2', cv2.imread(self.initialImg))
-        _main_ = MainModule()
-        _main_.get_facial_convex(image,imageObj)
+        #cv2.imshow('AUSGANGSBILD 1', image)
+        #cv2.imshow('AUSGANGSBILD 2', cv2.imread(self.initialImg))
+        photoLogic = PhotoLogic()
+        photoLogic.get_facial_convex(image,imageObj)
 
     def onButton4(self):
         image = cv2.imread(self.initialImg)
         copy = image
         target = cv2.imread(self.targetImg)
-        cv2.imshow('AUSGANGSBILD 1', image)
-        cv2.imshow('AUSGANGSBILD 2', target)
-        _main_ = MainModule()
-        result = _main_.get_faces(image,copy,target)
+        #cv2.imshow('AUSGANGSBILD 1', image)
+        #cv2.imshow('AUSGANGSBILD 2', target)
+        photoLogic = PhotoLogic()
+        result = photoLogic.get_faces(image,copy,target)
         cv2.imshow('Ergebnis',result)
 
-    def onButton5(self):
+    def onButton5(self,master,firstImg,secondImg):
+
         imageObj = Image.open('images/IMG_3597.JPG') 
         image = cv2.imread('images/IMG_3598.JPG')
-        cv2.imshow('AUSGANGSBILD 1', image)
-        cv2.imshow('AUSGANGSBILD 2', cv2.imread('images/IMG_3597.JPG'))
-        _main_ = MainModule()
-        _main_.get_facial_convex(image,imageObj)
+        
+        self.initialImg = 'images/IMG_3597.JPG'
+        self.targetImg = 'images/IMG_3598.JPG'
+        firstImg.set('images/IMG_3597.JPG')
+        secondImg.set('images/IMG_3598.JPG')
+            
+
+        if self.noFirstPreviewFrame:
+            self.firstPreviewFrame = Frame(master).pack(side = BOTTOM)
+            self.noFirstPreviewFrame = False      
+        else: 
+            self.firstPreviewLabel.destroy()
+        # create an image by using the filename-URL
+        tmp = Image.open('images/IMG_3597.JPG')
+        tmp = tmp.resize((450, 300), Image.ANTIALIAS)
+        theImage = ImageTk.PhotoImage(tmp)
+        # destroy the old label: delete all references to the old image
+        self.firstPreviewLabel = Label(self.firstPreviewFrame, image = theImage,text ="Image 1", compound=CENTER,font=("Helvetica", 30))
+        # make sure the references are correct:
+        self.firstPreviewLabel.image = theImage
+        # pack the label with out image in the frame for preview:
+        self.firstPreviewLabel.pack(side = LEFT, fill = BOTH, expand = YES)        
+        
+        if self.noSecondPreviewFrame:
+            self.secondPreviewFrame = Frame(master).pack(side = BOTTOM)
+            self.noSecondPreviewFrame = False      
+        else: 
+            self.secondPreviewLabel.destroy()
+        # create an image by using the filename-URL
+        tmp = Image.open('images/IMG_3598.JPG')
+        tmp = tmp.resize((450, 300), Image.ANTIALIAS)
+        theImage = ImageTk.PhotoImage(tmp)
+        # destroy the old label: delete all references to the old image
+        self.secondPreviewLabel = Label(self.secondPreviewFrame, image = theImage,text ="Image 2", compound=CENTER,font=("Helvetica", 30))
+        # make sure the references are correct:
+        self.secondPreviewLabel.image = theImage
+        # pack the label with out image in the frame for preview:
+        self.secondPreviewLabel.pack(side = RIGHT, fill = BOTH, expand = YES)
+
+
+
+
+
+
+
+
+
+        #cv2.imshow('AUSGANGSBILD 1', image)
+        #cv2.imshow('AUSGANGSBILD 2', cv2.imread('images/IMG_3597.JPG'))
+        photoLogic = PhotoLogic()
+        photoLogic.get_facial_convex(image,imageObj)
     
 root = generateWindow()
 gpe = GroupPhotoEditor(root)
---
+++ TestApplications/JunkFiles/PasteRectangles.py
@@ -0,0 +1,79 @@
+import cv2
+import dlib
+import numpy
+import sys
+import pdb
+
+PREDICTOR_PATH = "shape_predictor_68_face_landmarks.dat"
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(PREDICTOR_PATH)
+face_cascade = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade/haarcascade_eye.xml')
+
+class NoFaces(Exception):
+    pass
+
+def get_faces (copy, im, target):
+    
+    #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face = face_cascade.detectMultiScale(copy, 1.3, 5)
+
+    for(x,y,w,h) in face:
+        x = int(round(x*0.9))
+        y = int(round(y*0.6))
+        w = int(round(w*1.5))
+        h = int(round(h*1.7))
+        
+        cv2.rectangle(im, (x,y),(x+w,y+h),(255,0,0),2)
+        crop_img = copy[y:y+h,x:x+w]
+        x_offset=y_offset=x
+        target[y_offset:y_offset+crop_img.shape[0], x_offset:x_offset+crop_img.shape[1]] = crop_img
+
+       # target[y:y+crop_img.shape[0], x:x+crop_img.shape[1]] = crop_img
+        #roi_gray = gray[y:y+h, x:x+w]
+        roi = im [y:y+h, x:x+w]
+        eye = eye_cascade.detectMultiScale(roi,minNeighbors = 3)
+        for(ex,ey,ew,eh)in eye:
+            cv2.rectangle(roi, (ex, ey),(ex+ew, ey+eh),(0,255,0),2)
+    return im
+
+def get_landmarks(im):
+    rects = detector(im, 1)
+    
+    if len(rects) == 0:
+        raise NoFaces
+
+    return numpy.matrix([[p.x, p.y] for p in predictor(im, rects[0]).parts()])
+
+def annotate_landmarks(im, landmarks):
+    im = im.copy()
+    for idx, point in enumerate(landmarks):
+        pos = (point[0, 0], point[0, 1])
+        cv2.putText(im, str(idx), pos,
+                    fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
+                    fontScale=0.4,
+                    color=(0, 0, 255))
+        cv2.circle(im, pos, 3, color=(0, 255, 255))
+    return im
+
+
+
+image = cv2.imread('images/test3.jpg')
+target = cv2.imread('images/test2.jpg')
+copy = image
+face = face_cascade.detectMultiScale(image, 1.3, 5)
+
+for(x,y,w,h) in face:
+    landmarks = get_landmarks(image)
+    image = annotate_landmarks(image, landmarks)
+#landmarks = get_landmarks(image)
+#image_with_landmarks = annotate_landmarks(image, landmarks)
+#image_with_fd_landmarks = get_faces(image_with_landmarks)
+image_with_fd_landmarks = get_faces(copy,image,target)
+
+cv2.imshow('AUSGANGSBILD', image_with_fd_landmarks)
+cv2.waitKey(0)
+cv2.imshow('ERGEBNIS', target)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
